942e4b87e22bb0e9c08ddb63553b8bccd0fefc3d
mnurullaher <mnurullaher@gmail.com>
2023-07-10 15:49:33 +0300

    chore

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -6,7 +6,7 @@
           <h3 class="text-2xl">
               <a href="/posts/{{$post->id}}">{{$post->title}}</a>
           </h3>
-          <div class="text-xs mt-4"><a href="/?user={{ strtolower($post->user->id) }}">{{"By " . $post->user->name}}</a></div>
+          <div class="text-xs mt-4"><a href="/?user={{ $post->user->id }}">{{"By " . $post->user->name}}</a></div>
       </div>
   </div>
 </x-card>
